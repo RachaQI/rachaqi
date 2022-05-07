f2e0a979b103e967b48ca3023cfafafa69317dd9
RachaQI <rachaqiunivesp@gmail.com>
2022-05-07 16:03:29 -0300

    RachaQI Models -> 07052022

--- paginas/admin.py
+++ paginas/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import PostGa, PostIot, PostMin
+
 # Register your models here.
+
+admin.site.register(PostGa)
+admin.site.register(PostIot)
+admin.site.register(PostMin)
--- paginas/models.py
+++ paginas/models.py
@@ -5,27 +5,30 @@ from django.db import models
 # Create your models here.
 
 class PostGa(models.Model):
-    author_ga = models.ForeignKey(User, on_delete=models.CASCADE)
-    title_ga = models.CharField(max_length=255)
-    slug_ga = models.SlugField(max_length=255, unique=True)
-    body_ga = models.TextField()
-    created_ga = models.DateTimeField(auto_now_add=True)
-    updated_ga = models.DateTimeField(auto_now=True)
+    author_ga = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Autor")
+    title_ga = models.CharField(max_length=255, verbose_name="Título")
+    slug_ga = models.SlugField(max_length=255, unique=True, verbose_name="Link")
+    body_ga = models.TextField(verbose_name="Texto")
+    created_ga = models.DateTimeField(auto_now_add=True, verbose_name="Criado")
+    updated_ga = models.DateTimeField(auto_now=True, verbose_name="Editado")
+
+    def __str__(self):
+        return
 
 
 class PostIot(models.Model):
-    author_iot = models.ForeignKey(User, on_delete=models.CASCADE)
-    title_iot = models.CharField(max_length=255)
-    slug_iot = models.SlugField(max_length=255, unique=True)
-    body_iot = models.TextField()
-    created_iot = models.DateTimeField(auto_now_add=True)
-    updated_iot = models.DateTimeField(auto_now=True)
+    author_iot = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Autor")
+    title_iot = models.CharField(max_length=255, verbose_name="Título")
+    slug_iot = models.SlugField(max_length=255, unique=True, verbose_name="Link")
+    body_iot = models.TextField(verbose_name="Texto")
+    created_iot = models.DateTimeField(auto_now_add=True, verbose_name="Criado")
+    updated_iot = models.DateTimeField(auto_now=True, verbose_name="Editado")
 
 
 class PostMin(models.Model):
-    author_min = models.ForeignKey(User, on_delete=models.CASCADE)
-    title_min = models.CharField(max_length=255)
-    slug_min = models.SlugField(max_length=255, unique=True)
-    body_min = models.TextField()
-    created_min = models.DateTimeField(auto_now_add=True)
-    updated_min = models.DateTimeField(auto_now=True)
+    author_min = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Autor")
+    title_min = models.CharField(max_length=255, verbose_name="Título")
+    slug_min = models.SlugField(max_length=255, unique=True, verbose_name="Link")
+    body_min = models.TextField(verbose_name="Texto")
+    created_min = models.DateTimeField(auto_now_add=True, verbose_name="Criado")
+    updated_min = models.DateTimeField(auto_now=True, verbose_name="Editado")
