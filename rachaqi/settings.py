e92fdf303a504e9cee425033a5ad903858087eaa
RachaQI <rachaqiunivesp@gmail.com>
2022-05-25 12:40:19 -0300

    Preparando Deploy

--- rachaqi/settings.py
+++ rachaqi/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 import django_on_heroku
 from decouple import config
+from dj_database_url import parse as dburl
 
 from django.contrib import staticfiles
 
@@ -25,12 +26,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3=+p)_(ag1&57(#fs-gnxsn4#_8_kn_7lcxg_-#l92vlbxbnvr'
+
+SECRET_KEY = config('SECRET_KEY', default='', cast=str)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'rachaqi.herokuapp.com']
 
 # Application definition
 
@@ -91,15 +93,10 @@ WSGI_APPLICATION = 'rachaqi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'rachaqi',
-        'USER': 'postgres',
-        'PASSWORD': '06975071',
-        'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
-        'PORT': '5432',
-    }
+    'default': config('DATABASE_URL')
+
 }
 
 
